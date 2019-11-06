ad13fa697ea113d5343a51b908d97c7a71ad0b0a
johan alexander sanchez aguirre <johanalex566@gmail.com>
2019-11-06 20:17:14 +0100

    add application insights

--- WpfApp/ViewModel/Helpers/WeatherHelper.cs
+++ WpfApp/ViewModel/Helpers/WeatherHelper.cs
@@ -1,7 +1,12 @@
 ﻿//https://developer.accuweather.com/apis
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -10,31 +15,67 @@ using WpfApp.Model;
 
 namespace WpfApp.ViewModel.Helpers
 {
-    public  class WeatherHelper
+    public class WeatherHelper
     {
         public const string tokenAPI = "lO8gLTfks65nDSjCGMNir2tuEV3zwQKp&q";
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}={1}";
         public const string WEATHER_CONDITION_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public string insightInstrumentationKey = ConfigurationManager.AppSettings["insightInstrumentationKey"];
+
+        static TelemetryClient telemetry = new TelemetryClient();
+        TelemetryConfiguration configuration = TelemetryConfiguration.Active;
+        public WeatherHelper() => telemetry.InstrumentationKey = insightInstrumentationKey;
 
         public static async Task<List<City>> GetCities(string query)
         {
+
             List<City> cities = new List<City>();
             string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, tokenAPI, query);
-         
+
             try
             {
-                using (HttpClient client = new HttpClient())
+                using (var operation = telemetry.StartOperation<RequestTelemetry>("operationName"))
                 {
-                    var response = await client.GetAsync(url);
+                    using (HttpClient client = new HttpClient())
+                    {
+                        Stopwatch stopWatch = new Stopwatch();
 
-                    string json = await response.Content.ReadAsStringAsync();
+                        stopWatch.Start();
 
-                    cities = JsonConvert.DeserializeObject<List<City>>(json);
+                        var response = await client.GetAsync(url);
+
+                        string json = await response.Content.ReadAsStringAsync();
+
+                        cities = JsonConvert.DeserializeObject<List<City>>(json);
+
+                        stopWatch.Stop();
+
+                        // Set properties of containing telemetry item--for example:
+                        operation.Telemetry.ResponseCode = response.StatusCode.ToString();
+
+                        // Optional: explicitly send telemetry item:
+                        telemetry.StopOperation(operation);
+
+                        TimeSpan ts = stopWatch.Elapsed;
+
+                        MetricTelemetry metric = new MetricTelemetry("GetCities", ts.Milliseconds);
+
+                        metric.Properties.Add("Code", "wpfApp");
+
+                        if (!string.IsNullOrEmpty(json))
+                        metric.Properties.Add("Status", "True");
+                        else
+                            metric.Properties.Add("Status", "False");
+
+                        telemetry.TrackMetric(metric);
+                    }
                 }
             }
             catch (Exception ex)
             {
+                telemetry.TrackTrace($"Error{ex.Message}", SeverityLevel.Information);
+                telemetry.TrackException(ex);
                 throw new Exception(ex.Message);
             }
             return cities;
@@ -43,7 +84,7 @@ namespace WpfApp.ViewModel.Helpers
         public static async Task<Weather> GetWeather(string cityKey)
         {
             Weather weather = new Weather();
-            string url = BASE_URL + string.Format(WEATHER_CONDITION_ENDPOINT,cityKey,tokenAPI);
+            string url = BASE_URL + string.Format(WEATHER_CONDITION_ENDPOINT, cityKey, tokenAPI);
             try
             {
                 using (HttpClient client = new HttpClient())
